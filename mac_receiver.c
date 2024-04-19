5705bc4327acc18b69cdbb22024769ced4af4c43
Sylvan Arnold <sylvan.arnold@hes-so.ch>
2024-04-19 11:48:32 +0200

    token working

--- mac_receiver.c
+++ mac_receiver.c
@@ -1,5 +1,23 @@
+#include "main.h"
+#include "stm32f7xx_hal.h"
 
 void MacReceiver(void *argument)
 {
-	// TODO
+	struct queueMsg_t message;
+	uint8_t *data;
+	while(true){
+
+		osMessageQueueGet(queue_macR_id,&message,NULL,osWaitForever);
+	
+		data = message.anyPtr;
+	
+		if(data[0] == 0xFF){// if we received a token frame
+			message.type = TOKEN;// change message type to token
+			osMessageQueuePut(queue_macS_id,&message,osPriorityNormal,osWaitForever);// we transmit the token to the Mac sender
+		}
+		else{
+			osMemoryPoolFree(memPool,message.anyPtr);
+		}
+		
+	}
 }
--- mac_sender.c
+++ mac_sender.c
@@ -1,23 +1,42 @@
 #include "main.h"
 #include "stm32f7xx_hal.h"
 
+	osMessageQueueId_t waitingQueueId;
+	void sendToken(){
+			uint8_t* token = osMemoryPoolAlloc(memPool,osWaitForever);// allocate memory to create new token		
+
+			for(int i = 0; i < TOKENSIZE;i++){
+				token[i+1] = gTokenInterface.station_list[i];
+			}
+			
+			struct queueMsg_t msg;// message to send
+			msg.anyPtr = token;
+			msg.type = TO_PHY;
+			token[0] = TOKEN_TAG; // put the token tag in the first byte
+			token[gTokenInterface.myAddress+1] = (1<<TIME_SAPI) + gTokenInterface.connected?(1<<CHAT_SAPI):0;// send our available sapi
+			osMessageQueuePut(queue_phyS_id,&msg,osPriorityNormal,osWaitForever);// put the token message in the queue physic Send
+	}
+	
+	
 
 void MacSender(void *argument)
 {
-	queueMsg_t message;
-	osMessageQueueGet(queue_macS_id,&message,osPriorityNormal,osWaitForever);
-	uint8_t* msg
+	struct queueMsg_t message;
+	uint8_t *data;
 	
+	const osMessageQueueAttr_t waitingQueueAttr = {
+	.name = "WaitingQueue"
+	};
+	
+	waitingQueueId = osMessageQueueNew(2,sizeof(struct queueMsg_t),&waitingQueueAttr);	
+	
+	while(true){
+	osMessageQueueGet(queue_macS_id,&message,NULL,osWaitForever);
+	data = message.anyPtr;
+		
 	switch(message.type){
 		case NEW_TOKEN:
-		{
-			msg = osMemoryPoolAlloc(memPool,osWaitForever);
-			queueMsg_t token;
-			token.anyPtr = msg;
-			msg[0] = TOKEN_TAG;
-			msg[gTokenInterface.myAddress+1] = (1<<TIME_SAPI) + gTokenInterface.connected?(1<<CHAT_SAPI):0;
-			osMessageQueuePut(queue_phyS_id,&token,osWaitForever);
-		}		
+			sendToken();
 		break;
 	  case START:
 			gTokenInterface.connected = true;
@@ -27,12 +46,33 @@ void MacSender(void *argument)
 			break;
 		case DATA_IND:
 			// TODO: put the data in a queue and wait to send the message
+			// check if the destination's SAPI is active or not
 			break;
 		case TOKEN:
-			// TODO: send all the data in the queue 
-			// and send token??
+		{
+			bool sendTokenList = false;
+			for(int i = 0; i < TOKENSIZE;i++){
+				if(gTokenInterface.station_list[i] != data[i+1]){
+					gTokenInterface.station_list[i] = data[i+1];
+					sendTokenList = true;
+				}
+			}
+			if(sendTokenList){
+					struct queueMsg_t tokenlistMessage;
+					tokenlistMessage.type = TOKEN_LIST;
+					osMessageQueuePut(queue_lcd_id,&tokenlistMessage,NULL,osWaitForever);
+			}
+			
+			// TODO: send messages
+			
+			sendToken();
+		}
 			break;
-		case DATABACK:
-			// TODO: put back the data in the queue and count (maximum 3 databack)
+		case DATABACK:// when a message that we have send has made the turn => we need to check if it is Ack or not
+			// if not Ack, send the message back in the wait queue. (3 times maximum)
 			break;
+	}
+		osMemoryPoolFree(memPool,message.anyPtr);
 }
+}
+
