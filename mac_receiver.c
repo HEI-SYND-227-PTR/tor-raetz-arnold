6c782ebe3d6f0ecfe9129b67a2ae62c2ccb749ac
Sylvan Arnold <122262848+Sylvan22@users.noreply.github.com>
2024-05-05 20:01:01 +0200

    Added some comments

--- mac_receiver.c
+++ mac_receiver.c
@@ -1,108 +1,128 @@
+/*
+ * File name: mac_receiver.c
+ * Authors: Guillaume Raetz, Sylvan Arnold
+ * Date: 05/05/2024
+ *
+ * Description:
+ * This file contains the code for the MAC receiver in a token ring communication system.
+ * It includes a function to calculate the checksum of a data array and a main function
+ * for the MAC receiver that receives messages, processes data and token frames,
+ * and sends messages to different queues depending on their content.
+ */
+
+// Includes
 #include "main.h"
 #include "stm32f7xx_hal.h"
-#include <string.h> 
-		
+#include <string.h>
+
+// Function that calculates the checksum of a data array by summing all the bytes and returning the result
 uint8_t calcCheckSum(uint8_t* data, uint8_t length){
-	uint8_t result = 0;
-	for(int i = 0;i < length;i++){
-		result += data[i];
-	}
-	return result;
+    uint8_t result = 0;
+    for(int i = 0;i < length;i++){
+        result += data[i];
+    }
+    return result;
 }
 
+
+// Main function for the MAC receiver, that receives messages, processes data and token frames
 void MacReceiver(void *argument)
 {
-	
-	struct queueMsg_t macRmessage;
-	uint8_t *rxData;
-	
-		while(true){
 
-		osMessageQueueGet(queue_macR_id,&macRmessage,(uint8_t*)osPriorityNormal,osWaitForever); // get message
-		rxData = macRmessage.anyPtr;// get data in the message
-	
-		if(rxData[0] == 0xFF){// if we received a token frame
-			uint8_t* ptr = osMemoryPoolAlloc(memPool,osWaitForever);// allocate memory		
-			struct queueMsg_t newMSG;
-			newMSG.type = TOKEN;
-			newMSG.anyPtr = ptr;
-			memcpy(ptr,rxData,17);// copy the data			
-			osMessageQueuePut(queue_macS_id,&newMSG,osPriorityNormal,osWaitForever);// we transmit the TOKEN message to the Mac sender's queue
-		}
-		else
-		{
-			// get the message's informations
-			uint8_t dataLength = rxData[2];
-			uint8_t receivedChecksum = (rxData[dataLength+3]&CHECKSUM_MASK);
-			uint8_t calculatedChecksum = (calcCheckSum(rxData,dataLength+3)<<2);
-			uint8_t sourceAddress = ((rxData[0] & ADDRESS_MASK)>>3);
-			uint8_t destAddress = ((rxData[1] & ADDRESS_MASK)>>3);
-			uint8_t sourcesapi = (rxData[0] & SAPI_MASK);
-			uint8_t destSapi = (rxData[1] & SAPI_MASK);
-			
-			// send to TIME or chat
-			if((destAddress == gTokenInterface.myAddress) || (destAddress == 0xF)){
-				if(calculatedChecksum == receivedChecksum){// check if CRC is correct
-					if(((destSapi == CHAT_SAPI) && (gTokenInterface.connected))||(destSapi == TIME_SAPI)){ // if the targeted sapi match with one of our active sapis
-							uint8_t* ptr = osMemoryPoolAlloc(memPool,osWaitForever);// allocate memory		
-							struct queueMsg_t newMSG;
-							newMSG.type = DATA_IND;
-							newMSG.addr = sourceAddress;// indicate the source's address
-							newMSG.sapi = sourcesapi;// indicate the source's SAPI
-							newMSG.anyPtr = ptr;
-							memcpy(ptr,&rxData[3],dataLength);// copy the data
-							ptr[dataLength] = 0;
-							//osMessageQueuePut((destSapi==CHAT_SAPI)?queue_chatR_id:queue_timeR_id,&newMSG,osPriorityNormal,osWaitForever);// send the message to the correct message queue
-							if(osMessageQueuePut((destSapi==CHAT_SAPI)?queue_chatR_id:queue_timeR_id,&newMSG,osPriorityNormal,0)!= osOK){// send the message to the correct message queue
-								osMemoryPoolFree(memPool,ptr);//release memory if we can't put theb message in the queue
-							}
-						}
-				}
-			}
-			
-			// databack
-			if(sourceAddress == gTokenInterface.myAddress){
-					uint8_t* ptr = osMemoryPoolAlloc(memPool,osWaitForever);// allocate memory		
-					struct queueMsg_t newMSG;
-					newMSG.type = DATABACK;
-					newMSG.anyPtr = ptr;
-					memcpy(ptr,rxData,dataLength+4);// copy the data
-				
-					// update read and ack bits if I was the sender
-					if((calculatedChecksum == receivedChecksum) && (destAddress == gTokenInterface.myAddress)){
-							ptr[dataLength+3] |= 0x3; // Put read and Ack bits to 1
-						}
-						else{
-							ptr[dataLength+3] |= 0x2; // Put read bit to 1 and Ack bit to 0
-					}
+    // Declaration of the message and the data
+    struct queueMsg_t macRmessage;
+    uint8_t *rxData;
 
-					osMessageQueuePut(queue_macS_id,&newMSG,osPriorityNormal,osWaitForever);//send to macS
-				
-			}		
-			// to physical layer
-			else{
+    while(true){
 
-					uint8_t* ptr = osMemoryPoolAlloc(memPool,osWaitForever);// allocate memory		
-					struct queueMsg_t newMSG;
-					newMSG.type = TO_PHY;
-					newMSG.anyPtr = ptr;
-					memcpy(ptr,rxData,dataLength+4);// copy the data
-					if(destAddress != 0xF){// if it is not a broadcast message
-						if(calculatedChecksum == receivedChecksum){
-							ptr[dataLength+3] |= 0x3; // Put read and Ack bits to 1
-						}
-						else{
-							ptr[dataLength+3] |= 0x2; // Put read bit to 1 and Ack bit to 0
-						}
-					}
-					
-					osMessageQueuePut(queue_phyS_id,&newMSG,osPriorityNormal,osWaitForever); // send back message to physic layer
-			}
+        osMessageQueueGet(queue_macR_id,&macRmessage,(uint8_t*)osPriorityNormal,osWaitForever); // get message
+        rxData = macRmessage.anyPtr;// get data in the message
 
-			
-		}
-		osMemoryPoolFree(memPool,rxData);// release the memory
-	}
+        if(rxData[0] == 0xFF){// if we received a token frame
+            uint8_t* ptr = osMemoryPoolAlloc(memPool,osWaitForever);// allocate memory
+            struct queueMsg_t newMSG;
+            newMSG.type = TOKEN;
+            newMSG.anyPtr = ptr;
+            memcpy(ptr,rxData,17);// copy the data
+            osMessageQueuePut(queue_macS_id,&newMSG,osPriorityNormal,osWaitForever);// we transmit the TOKEN message to the Mac sender's queue
+        }
+        else //if we received a data frame
+        {
+            // get the message's informations in the data frame using masks and shifts
+            uint8_t dataLength = rxData[2];
+            uint8_t receivedChecksum = (rxData[dataLength+3]&CHECKSUM_MASK);
+            uint8_t calculatedChecksum = (calcCheckSum(rxData,dataLength+3)<<2);
+            uint8_t sourceAddress = ((rxData[0] & ADDRESS_MASK)>>3);
+            uint8_t destAddress = ((rxData[1] & ADDRESS_MASK)>>3);
+            uint8_t sourcesapi = (rxData[0] & SAPI_MASK);
+            uint8_t destSapi = (rxData[1] & SAPI_MASK);
+
+            // send to TIME or chat
+            // DATA_IND
+            if((destAddress == gTokenInterface.myAddress) || (destAddress == 0xF)){ // if the message is for me or for everyone (broadcast)
+                if(calculatedChecksum == receivedChecksum){// check if CRC is correct
+                    if(((destSapi == CHAT_SAPI) && (gTokenInterface.connected))||(destSapi == TIME_SAPI)){ // if the targeted sapi match with one of our active sapis
+                        uint8_t* ptr = osMemoryPoolAlloc(memPool,osWaitForever);// allocate memory
+                        struct queueMsg_t newMSG;
+                        newMSG.type = DATA_IND; // the type of the message is a data indication, its destination is time receiver or chat receiver
+                        newMSG.addr = sourceAddress;// indicate the source's address
+                        newMSG.sapi = sourcesapi;// indicate the source's SAPI
+                        newMSG.anyPtr = ptr;
+                        memcpy(ptr,&rxData[3],dataLength);// copy the data
+                        ptr[dataLength] = 0; // add a null character at the end of the string
+                        //osMessageQueuePut((destSapi==CHAT_SAPI)?queue_chatR_id:queue_timeR_id,&newMSG,osPriorityNormal,osWaitForever);// send the message to the correct message queue
+                        if(osMessageQueuePut((destSapi==CHAT_SAPI)?queue_chatR_id:queue_timeR_id,&newMSG,osPriorityNormal,0)!= osOK){// send the message to the correct message queue
+                            osMemoryPoolFree(memPool,ptr);//release memory if we can't put theb message in the queue
+                        }
+                    }
+                }
+            }
+
+            // DATABACK
+            if(sourceAddress == gTokenInterface.myAddress){ // if I was the sender
+                uint8_t* ptr = osMemoryPoolAlloc(memPool,osWaitForever);// allocate memory
+                struct queueMsg_t newMSG;
+                newMSG.type = DATABACK;
+                newMSG.anyPtr = ptr;
+                memcpy(ptr,rxData,dataLength+4);// copy the data
+
+                // update read and ack bits if the message was for me
+                if((calculatedChecksum == receivedChecksum) && (destAddress == gTokenInterface.myAddress)){
+                    ptr[dataLength+3] |= 0x3; // Put read and Ack bits to 1
+                }
+                else
+                {
+                    ptr[dataLength+3] |= 0x2; // Put read bit to 1 and Ack bit to 0
+                }
+
+                osMessageQueuePut(queue_macS_id,&newMSG,osPriorityNormal,osWaitForever);//send to macS
+
+            }
+                // to physical layer if i was not the sender
+            else{
+
+                uint8_t* ptr = osMemoryPoolAlloc(memPool,osWaitForever);// allocate memory
+                struct queueMsg_t newMSG;
+                newMSG.type = TO_PHY;
+                newMSG.anyPtr = ptr;
+                memcpy(ptr,rxData,dataLength+4);// copy the data
+                if(destAddress != 0xF){// if it is not a broadcast message
+                    if(calculatedChecksum == receivedChecksum)
+                    {
+                        ptr[dataLength+3] |= 0x3; // Put read and Ack bits to 1
+                    }
+                    else
+                    {
+                        ptr[dataLength+3] |= 0x2; // Put read bit to 1 and Ack bit to 0
+                    }
+                }
+                osMessageQueuePut(queue_phyS_id,&newMSG,osPriorityNormal,osWaitForever); // send back message to physic layer
+            }
+
+
+        }
+        osMemoryPoolFree(memPool,rxData);// release the memory
+    }
 }
 
 
--- mac_sender.c
+++ mac_sender.c
@@ -1,196 +1,213 @@
+/*
+ * File name: mac_sender.c
+ * Authors: Guillaume Raetz, Sylvan Arnold
+ * Date: 05/05/2024
+ *
+ * Description:
+    * This file contains the mac sender thread. It is used to send data to the physic layer.
+    * It also sends token frames and new token frames.
+    * It also sends error messages to the LCD when we can't send a message to a specific station.
+    * It also sends token list messages to the LCD when the token list has changed.
+    * It also sends back data frames when they are not acknowledged.
+    * It also sends data frames to the physic layer.
+ */
+
+// Includes
+
 #include "main.h"
 #include "stm32f7xx_hal.h"
-#include <string.h> 
+#include <string.h>
 
-	osMessageQueueId_t waitingQueueId; // waiting queue used to store all the DATA_IND message before we can send them
-	
-	
-	// this function send a token frame when called.
-	void sendToken(){
-			uint8_t* token = osMemoryPoolAlloc(memPool,osWaitForever);// allocate memory to create new token		
+osMessageQueueId_t waitingQueueId; // waiting queue used to store all the DATA_IND message before we can send them
 
-			for(int i = 0; i < 15;i++){// get all the stations's SAPI state and put them in the frame
-				token[i+1] = gTokenInterface.station_list[i];
-			}
-			
-			struct queueMsg_t msg;// message to send
-			msg.anyPtr = token;
-			msg.type = TO_PHY;
-			token[0] = TOKEN_TAG; // put the token tag in the first byte
-			
-			token[gTokenInterface.myAddress+1] = (1<<TIME_SAPI);
-			if(gTokenInterface.connected){
-				token[gTokenInterface.myAddress+1] |= (1<<CHAT_SAPI);
-			}
-			
-			osMessageQueuePut(queue_phyS_id,&msg,osPriorityNormal,osWaitForever);// put the token message in the queue physic Send
-	}
-	
-	// this function send a new token frame in the ring
-	void sendNewToken(){
-		uint8_t* token = osMemoryPoolAlloc(memPool,osWaitForever);// allocate memory to create new token		
 
-			for(int i = 0; i < 15;i++){// reset memory to 0
-				token[i+1] = 0;
-			}
-			
-			struct queueMsg_t msg;// message to send
-			msg.anyPtr = token;
-			msg.type = TO_PHY;
-			token[0] = TOKEN_TAG; // put the token tag in the first byte
-			
-			token[gTokenInterface.myAddress+1] = (1<<TIME_SAPI);
-			token[gTokenInterface.myAddress+1] |= (1<<CHAT_SAPI);// by default, I say that chat SAPI is connected
+// this function send a token frame when called.
+void sendToken(){
+    uint8_t* token = osMemoryPoolAlloc(memPool,osWaitForever);// allocate memory to create new token
 
-			
-			osMessageQueuePut(queue_phyS_id,&msg,osPriorityNormal,osWaitForever);// put the token message in the queue physic Send
-	}
-	
+    for(int i = 0; i < 15;i++){// get all the stations's SAPI state and put them in the frame
+        token[i+1] = gTokenInterface.station_list[i];
+    }
+
+    struct queueMsg_t msg;// message to send
+    msg.anyPtr = token;
+    msg.type = TO_PHY;
+    token[0] = TOKEN_TAG; // put the token tag in the first byte
+
+
+    token[gTokenInterface.myAddress+1] = (1<<TIME_SAPI);
+    if(gTokenInterface.connected){
+        token[gTokenInterface.myAddress+1] |= (1<<CHAT_SAPI);
+    }
+
+    osMessageQueuePut(queue_phyS_id,&msg,osPriorityNormal,osWaitForever);// put the token message in the queue physic Send
+}
+
+// this function send a new token frame in the ring
+void sendNewToken(){
+    uint8_t* token = osMemoryPoolAlloc(memPool,osWaitForever);// allocate memory to create new token
+
+    for(int i = 0; i < 15;i++){// reset memory to 0
+        token[i+1] = 0;
+    }
+
+    struct queueMsg_t msg;// message to send
+    msg.anyPtr = token;
+    msg.type = TO_PHY;
+    token[0] = TOKEN_TAG; // put the token tag in the first byte
+
+    token[gTokenInterface.myAddress+1] = (1<<TIME_SAPI);
+    token[gTokenInterface.myAddress+1] |= (1<<CHAT_SAPI);// by default, I say that chat SAPI is connected
+
+
+    osMessageQueuePut(queue_phyS_id,&msg,osPriorityNormal,osWaitForever);// put the token message in the queue physic Send
+}
+
 // function used to calculate the checksum of the data putted in entry	
 uint8_t calcuCheckSum(uint8_t* data, uint8_t length){
-	uint8_t result = 0;
-	for(int i = 0;i < length;i++){
-		result += data[i];
-	}
-	return result;
+    uint8_t result = 0;
+    for(int i = 0;i < length;i++){
+        result += data[i];
+    }
+    return result;
 }
-	
+
 // function used to send an error to the LCD when we can't send a message to a specific station
 void sendMacError(){
-		struct queueMsg_t msg;// message to send
-		uint8_t* ptr = NULL;
-		uint8_t* message = "Error, could not send message\n";
-	
-		ptr = osMemoryPoolAlloc(memPool,osWaitForever);// allocate memory 
-		if(ptr != NULL){	
-			memcpy(ptr,message,31);	// copy the message in ptr
-			msg.anyPtr = ptr;
-			msg.type = MAC_ERROR;
-			osMessageQueuePut(queue_lcd_id,&msg,osPriorityNormal,osWaitForever);//send message
-		}
+    struct queueMsg_t msg;// message to send
+    uint8_t* ptr = NULL;
+    uint8_t* message = "Error, could not send message\n";
+
+    ptr = osMemoryPoolAlloc(memPool,osWaitForever);// allocate memory
+    if(ptr != NULL){
+        memcpy(ptr,message,31);	// copy the message in ptr
+        msg.anyPtr = ptr;
+        msg.type = MAC_ERROR;
+        osMessageQueuePut(queue_lcd_id,&msg,osPriorityNormal,osWaitForever);//send message
+    }
 }
 
 // this function sends all the data in the waiting queue and when there is no more data to send, it calls the sendtoken funcion
 void sendData(){
-		struct queueMsg_t dataToSend;
-		if(osMessageQueueGet(waitingQueueId,&dataToSend,(uint8_t*)osPriorityNormal,0) == osOK){
-			
-		uint8_t* ptr = dataToSend.anyPtr;
-			
-			osMessageQueuePut(queue_phyS_id,&dataToSend,osPriorityNormal,osWaitForever);
-		}
-		else{
-			sendToken();
-		}
+    struct queueMsg_t dataToSend;
+    if(osMessageQueueGet(waitingQueueId,&dataToSend,(uint8_t*)osPriorityNormal,0) == osOK){
+
+        uint8_t* ptr = dataToSend.anyPtr;
+
+        osMessageQueuePut(queue_phyS_id,&dataToSend,osPriorityNormal,osWaitForever);
+    }
+    else{
+        sendToken();
+    }
 }
 
 void MacSender(void *argument)
 {
-	struct queueMsg_t macSmessage;
-	uint8_t *data;
-	
-	const osMessageQueueAttr_t waitingQueueAttr = {
-	.name = "WaitingQueue"
-	};
-	waitingQueueId = osMessageQueueNew(4,sizeof(struct queueMsg_t),&waitingQueueAttr);	
-	
-	uint8_t nResend = 0;// used to count how many times we have resent a message non acknowledged
-	
-	while(true){
-	osMessageQueueGet(queue_macS_id,&macSmessage,(uint8_t*)osPriorityNormal,osWaitForever);
-	data = macSmessage.anyPtr;
-		
-	switch(macSmessage.type){
-		case NEW_TOKEN:
-			sendNewToken();// send a new token
-		break;
-	  case START:
-			gTokenInterface.connected = true;
-			gTokenInterface.station_list[gTokenInterface.myAddress] = gTokenInterface.station_list[gTokenInterface.myAddress] | (1<<CHAT_SAPI);//set chat SAPI's bit
-			break;
-		case STOP:
-			gTokenInterface.connected = false;
-			gTokenInterface.station_list[gTokenInterface.myAddress] = (gTokenInterface.station_list[gTokenInterface.myAddress] & 0xFD);//reset chat SAPI's bit
-			break;
-		case DATA_IND:
-		{
-			//Create a data Frame with DATA_IND's data and put it in the wait message queue
-			
-			uint8_t* ptr = osMemoryPoolAlloc(memPool,osWaitForever);// allocate some memory
-			
-			ptr[0] = (gTokenInterface.myAddress << 3) | macSmessage.sapi;	// control 1 (source Address + source SAPI)
-			ptr[1] = (macSmessage.addr <<3) | macSmessage.sapi; 							//control 2 (dest. address + dest. SAPI)
-			
-			// copy the data and calculate length
-			uint8_t length = 0;
-			while(data[length] != '\0'){
-				ptr[length+3] = data[length];
-				length++;
-			}
-			ptr[length+3] = '\0'; // also put \0 in the end of the data
-			
-			ptr[2] = (length+1);// add length in the frame
-			
-			uint8_t checksum = calcuCheckSum(ptr,length+3);// calculate the checksum
-				
-			ptr[length+4] = checksum << 2;// status (checksum + Read bit + Ack bit)
-			
-			struct queueMsg_t msg;
-			msg.anyPtr = ptr;
-			msg.type = TO_PHY;
-			
-					
-			//osMessageQueuePut(waitingQueueId,&msg,osPriorityNormal,osWaitForever);
-			if(osMessageQueuePut(waitingQueueId,&msg,osPriorityNormal,0) != osOK){// if we can't put the message in the queue
-				osMemoryPoolFree(memPool,data);//release memory
-			}
-		}
-			break;
-		case TOKEN:
-		{
-			bool sendTokenList = false;// flage used to know if we have to send a tokenList message to the lcd
-			for(int i = 0; i < 15;i++){ //Update station's available sapis
-				if(gTokenInterface.station_list[i] != data[i+1]){
-					gTokenInterface.station_list[i] = data[i+1];
-					sendTokenList = true;
-				}
-			}
-			if(sendTokenList){// if the token list has changed since the last time, we advertise the LCD by sending a TOKEN_LIST message
-					struct queueMsg_t tokenlistMessage;
-					tokenlistMessage.type = TOKEN_LIST;
-					osMessageQueuePut(queue_lcd_id,&tokenlistMessage,osPriorityNormal,osWaitForever);
-			}
-			sendData();			
-			
-		}
-			break;
-		case DATABACK:
-		{
-			struct queueMsg_t dataToSend;
-			uint8_t dataLength = data[2];
-			uint8_t readAck = (data[dataLength+3] & ACK_READ_MASK);
-			uint8_t destAddress = ((data[1] & ADDRESS_MASK)>>3);
-		if((readAck != 0x3) && (nResend < 2) && (destAddress != 0xF)){// check read and ack bit, check nResend (must not be = 2), check that it is not a broadcast frame
-			// if the message is not acknowledged
-			nResend ++;
-			data[dataLength+3] = (data[dataLength+3] & 0xFC); // reset read and ack bits
-			uint8_t *ptr = osMemoryPoolAlloc(memPool,osWaitForever);
-			memcpy(ptr,data,dataLength+4);
-			macSmessage.anyPtr = ptr;
-			osMessageQueuePut(queue_phyS_id,&macSmessage,osPriorityNormal,osWaitForever);// send back the message
-		}
-		else{
-				if(nResend == 2){
-					sendMacError();
-				}
-				nResend = 0;// reset the counter
-				sendData();
-			}				
-		}			
-			break;
-	}
-		osMemoryPoolFree(memPool,data);// release the memory
-}
+    struct queueMsg_t macSmessage;
+    uint8_t *data;
+
+    const osMessageQueueAttr_t waitingQueueAttr = {
+            .name = "WaitingQueue"
+    };
+    waitingQueueId = osMessageQueueNew(4,sizeof(struct queueMsg_t),&waitingQueueAttr);
+
+    uint8_t nResend = 0;// used to count how many times we have resent a message non acknowledged
+
+    while(true){
+        osMessageQueueGet(queue_macS_id,&macSmessage,(uint8_t*)osPriorityNormal,osWaitForever);
+        data = macSmessage.anyPtr;
+
+        switch(macSmessage.type){
+            case NEW_TOKEN:
+                sendNewToken();// send a new token
+                break;
+            case START:
+                gTokenInterface.connected = true;
+                gTokenInterface.station_list[gTokenInterface.myAddress] = gTokenInterface.station_list[gTokenInterface.myAddress] | (1<<CHAT_SAPI);//set chat SAPI's bit
+                break;
+            case STOP:
+                gTokenInterface.connected = false;
+                gTokenInterface.station_list[gTokenInterface.myAddress] = (gTokenInterface.station_list[gTokenInterface.myAddress] & 0xFD);//reset chat SAPI's bit
+                break;
+            case DATA_IND:
+            {
+                //Create a data Frame with DATA_IND's data and put it in the wait message queue
+
+                uint8_t* ptr = osMemoryPoolAlloc(memPool,osWaitForever);// allocate some memory
+
+                ptr[0] = (gTokenInterface.myAddress << 3) | macSmessage.sapi;	// control 1 (source Address + source SAPI)
+                ptr[1] = (macSmessage.addr <<3) | macSmessage.sapi; 							//control 2 (dest. address + dest. SAPI)
+
+                // copy the data and calculate length
+                uint8_t length = 0;
+                while(data[length] != '\0'){
+                    ptr[length+3] = data[length];
+                    length++;
+                }
+                ptr[length+3] = '\0'; // also put \0 in the end of the data
+
+                ptr[2] = (length+1);// add length in the frame
+
+                uint8_t checksum = calcuCheckSum(ptr,length+3);// calculate the checksum
+
+                ptr[length+4] = checksum << 2;// status (checksum + Read bit + Ack bit)
+
+                struct queueMsg_t msg;
+                msg.anyPtr = ptr;
+                msg.type = TO_PHY;
+
+
+                //osMessageQueuePut(waitingQueueId,&msg,osPriorityNormal,osWaitForever);
+                if(osMessageQueuePut(waitingQueueId,&msg,osPriorityNormal,0) != osOK){// if we can't put the message in the queue
+                    osMemoryPoolFree(memPool,data);//release memory
+                }
+            }
+                break;
+            case TOKEN:
+            {
+                bool sendTokenList = false;// flage used to know if we have to send a tokenList message to the lcd
+                for(int i = 0; i < 15;i++){ //Update station's available sapis
+                    if(gTokenInterface.station_list[i] != data[i+1]){
+                        gTokenInterface.station_list[i] = data[i+1];
+                        sendTokenList = true;
+                    }
+                }
+                if(sendTokenList){// if the token list has changed since the last time, we advertise the LCD by sending a TOKEN_LIST message
+                    struct queueMsg_t tokenlistMessage;
+                    tokenlistMessage.type = TOKEN_LIST;
+                    osMessageQueuePut(queue_lcd_id,&tokenlistMessage,osPriorityNormal,osWaitForever);
+                }
+                sendData();
+
+            }
+                break;
+            case DATABACK:
+            {
+                struct queueMsg_t dataToSend;
+                uint8_t dataLength = data[2];
+                uint8_t readAck = (data[dataLength+3] & ACK_READ_MASK);
+                uint8_t destAddress = ((data[1] & ADDRESS_MASK)>>3);
+                if((readAck != 0x3) && (nResend < 2) && (destAddress != 0xF)){// check read and ack bit, check nResend (must not be = 2), check that it is not a broadcast frame
+                    // if the message is not acknowledged
+                    nResend ++;
+                    data[dataLength+3] = (data[dataLength+3] & 0xFC); // reset read and ack bits
+                    uint8_t *ptr = osMemoryPoolAlloc(memPool,osWaitForever);
+                    memcpy(ptr,data,dataLength+4);
+                    macSmessage.anyPtr = ptr;
+                    osMessageQueuePut(queue_phyS_id,&macSmessage,osPriorityNormal,osWaitForever);// send back the message
+                }
+                else{
+                    if(nResend == 2){
+                        sendMacError();
+                    }
+                    nResend = 0;// reset the counter
+                    sendData();
+                }
+            }
+                break;
+        }
+        osMemoryPoolFree(memPool,data);// release the memory
+    }
 }
 
 
